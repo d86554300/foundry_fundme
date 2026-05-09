cfd4c06f11c639331b7565ff36adf22c914b513f
Leonardo Ribeiro <d86554300@gmail.com>
2026-05-09 00:02:34 -0300

    Code refactor for Mock support and dynamic deploy of contract

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -4,13 +4,13 @@ pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
 import "../src/FundMe.sol";
+import {HelperConfig} from "../script/HelperConfig.s.sol";
 
 contract DeployFundMe is Script {
     function run() external returns (FundMe) {
+        HelperConfig helperConfig = new HelperConfig();
         vm.startBroadcast();
-        FundMe fundMeContract = new FundMe(
-            0x694AA1769357215DE4FAC081bf1f309aDC325306
-        );
+        FundMe fundMeContract = new FundMe(helperConfig.activeNetworkConfig());
         vm.stopBroadcast();
         return fundMeContract;
     }
